bcd13a51e0073acd30e24495703ac8dca0692e2b
Shir <shir.avraham13@gmail.com>
2019-12-02 22:08:35 +0200

    added build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,19 +3,6 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const burger = props => {
-  // let transformedIngredients = Object.keys(props.ingredients)
-  // .map(igKey => {
-  //   return [...Array(props.ingredients[igKey])]
-  //     .map((_, i) => {
-  //       return (
-  //         <BurgerIngredient key={igKey + 1} type={igKey}></BurgerIngredient>
-  //       );
-  //     })
-  //     .reduce((arr, el) => {
-  //       return arr.concat(el);
-  //     }, []);
-  // });
-
   let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
