41026ab64c6e35adb5b5f80c79ce143a15ca229b
Shir <shir.avraham13@gmail.com>
2019-12-04 23:40:35 +0200

    connected state to build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,13 @@ const buildControls = props => {
   return (
     <div className={classes.BuildControls}>
       {controls.map(control => (
-        <BuildControl key={control.labbel} label={control.label} />
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => props.ingredientAdded(control.type)}
+          removed={() => props.ingredientRemoved(control.type)}
+          disabled={props.disabled[control.type]}
+        />
       ))}
     </div>
   );
