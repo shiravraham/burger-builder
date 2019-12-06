d3e781cf203be3a723c6b2c605e7d7e16045ed1b
Shir <shir.avraham13@gmail.com>
2019-12-06 16:45:50 +0200

    Added logo

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const navigationItems = props => {
+  return (
+    <ul>
+      <li>
+        <a href="/">A link</a>
+      </li>
+    </ul>
+  );
+};
+
+export default navigationItems;
